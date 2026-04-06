409064533ed0b6eec1762d6098e30be4e3fa57e5
Aria Pessianzadeh <81302877+aryapess@users.noreply.github.com>
2026-04-06 14:51:31 -0400

    Update title for cancer misinformation annotation

--- app_misinfo.py
+++ app_misinfo.py
@@ -9,7 +9,7 @@ import streamlit as st
 st.set_page_config(page_title="Cancer Misinformation Annotation", layout="wide")
 
 # Everything else goes below
-st.title("Annotate JSON 'body'")
+st.title("Cancer Misinformation Annotation")
 
 # -----------------------------
 # Config
